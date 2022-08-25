3002b28552a1d8fc3262d50fac9a7c6e2abc851c
Mohammad Imran <mohammad.imran@diverselynx.com>
2022-08-25 19:57:16 +0530

    Test
    Signed-off-by: Mohammad Imran <mohammad.imran@diverselynx.com>

--- src/Pages/cart.js
+++ src/Pages/cart.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Cart =()=>{
-
+const Cart =({state, dispatch})=>{
+    const {cart, paroducts} = state;
+    const [total, setTotal] = useState(0)
     return (
         <>
       <div className="cart-container">
+        <h2 className="cart-heading">Cart Info</h2>
+        <p>Total : {total}</p>
 
-    <h1>Cart Info</h1>
+        {
+            cart.length > 0 ? 
+            <div>
+                {
+                    cart.map((prod, i)=>
+                        <div className="cart-prod"> 
+                        <img src={prod.thumbnail} alt={prod.title}/>
+                        </div>
+                    
+                    )
+                }
+            </div>
+            : 
+            
+            <p>Your cart is empty</p>
+        }
       </div>
         </>
     )
 }
 
 export default Cart
+
